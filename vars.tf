1fbcd11fad0e8e9f40e3c3d22f19a5d3f8ee1b0d
sirisha517 <sai.sirifriends8790@gmail.com>
2023-04-10 22:00:16 +0530

    rabbitmq

---
+++ vars.tf
@@ -0,0 +1,9 @@
+variable "env" {}
+variable "tags" {}
+variable "name" {}
+variable "internal" {}
+variable "load_balancer_type" {}
+variable "enable_deletion_protection" {
+  default = false
+}
+variable "subnets" {}
