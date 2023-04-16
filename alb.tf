39846dab3fce12e5f734712656eba162e3140c4a
sirisha517 <sai.sirifriends8790@gmail.com>
2023-04-16 12:41:45 +0530

    open application port

--- alb.tf
+++ alb.tf
@@ -3,7 +3,7 @@ resource "aws_lb" "main" {
   internal           = var.internal
   load_balancer_type = var.load_balancer_type
   subnets            = var.subnets
-
+  security_groups    = [aws_security_group.main.id]
   enable_deletion_protection = var.enable_deletion_protection
 
   tags = merge(
