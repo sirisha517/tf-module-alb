c179d99271c2560df4d925a25dcdb5f3f5fcf170
sirisha517 <sai.sirifriends8790@gmail.com>
2023-04-18 18:44:10 +0530

    open application port

--- alb.tf
+++ alb.tf
@@ -1,10 +1,10 @@
 resource "aws_lb" "main" {
-  name               = "${var.name}-${var.env}"
-  internal           = var.internal
-  load_balancer_type = var.load_balancer_type
-  subnets            = var.subnets
-  security_groups    = [aws_security_group.main.id]
+  name                       = "${var.name}-${var.env}"
+  internal                   = var.internal
+  load_balancer_type         = var.load_balancer_type
+  subnets                    = var.subnets
   enable_deletion_protection = var.enable_deletion_protection
+  security_groups            = [aws_security_group.main.id]
 
   tags = merge(
     var.tags,
@@ -16,6 +16,7 @@ resource "aws_security_group" "main" {
   name        = "${var.name}-${var.env}-lb"
   description = "${var.name}-${var.env}-lb"
   vpc_id      = var.vpc_id
+
   ingress {
     description = "APP"
     from_port   = 80
@@ -23,6 +24,7 @@ resource "aws_security_group" "main" {
     protocol    = "tcp"
     cidr_blocks = var.allow_cidr
   }
+
   egress {
     from_port        = 0
     to_port          = 0
@@ -30,8 +32,9 @@ resource "aws_security_group" "main" {
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
   }
+
   tags = merge(
     var.tags,
-    { Name = "${var.name}-${var.env}-lb"}
+    { Name = "${var.name}-${var.env}-lb" }
   )
 }
